ba82f5295fbf7f2789b87b6471dd0b204b492ee9
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-30 22:55:51 -0500

    create squares for board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Board from './board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chess
-        </p>
-      </header>
+      <Board />
     </div>
   );
 }
